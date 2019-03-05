7a81081eea7d1c4cf3655a804500a907b1e0d53c
Jaxc <Jaxc@users.noreply.github.com>
2019-03-05 13:32:23 +0100

    Update beat detection algoritm
    Added some much needed updates to beat detection

--- beat_detection.c
+++ beat_detection.c
@@ -7,26 +7,39 @@ uint8_t old_values_pnt = 0;
 #define THRESHHOLD 100000
 
 uint32_t beat_buffer[WINDOW_SIZE] = {0};
+uint32_t pwr_sum_buffer[WINDOW_SIZE*2] = {0};
 uint32_t beat_buffer_pnt = 0;
 
-void calc_power (int32_t new_sample, uint32_t *power_out) {
+void calc_power (int32_t new_sample, uint32_t *power_out, uint32_t *debug_out) {
     old_values[old_values_pnt] = new_sample >> 8;
     old_values_pnt = (old_values_pnt + 1) % N_AVG;
     
     uint64_t pwr_sum = 0; 
     
-    for (int i = 0; i < N_AVG; i++) {
+    for (uint32_t i = 0; i < N_AVG; i++) {
         pwr_sum += old_values[i] * old_values[i];
     }
     
+    pwr_sum_buffer[beat_buffer_pnt % (WINDOW_SIZE >> 4)] = pwr_sum;
+    
+    uint32_t median_pwr = 0;
+    for (uint32_t i = 0; i < WINDOW_SIZE; ++i) {
+        median_pwr += pwr_sum_buffer[i] >> 7;
+    }
+    
     if (pwr_sum < THRESHHOLD) {
+        pwr_sum = 0;
+    } else if (pwr_sum < median_pwr ) {
         pwr_sum = 0;
     }
     
     beat_buffer[beat_buffer_pnt] = (uint32_t)pwr_sum;
     beat_buffer_pnt = (beat_buffer_pnt + 1) % WINDOW_SIZE;
     
-    *power_out = bpm_detect(beat_buffer);
+    //*power_out = median_pwr; 
+    *power_out = bpm_detect(beat_buffer)*10000;
+    *debug_out = pwr_sum;
+    
     
     
 }
@@ -34,7 +47,7 @@ void calc_power (int32_t new_sample, uint32_t *power_out) {
 uint32_t bpm_detect(uint32_t window_in[WINDOW_SIZE]) {
     
     detection_states state = WAIT_FOR_FIRST_SAMPLE;
-    uint32_t beats_start[100] = {0};
+    uint32_t beats_start[1000] = {0};
     uint32_t n_beats = 0;
     uint8_t trans_state_cnt = 0;
     
@@ -45,7 +58,7 @@ uint32_t bpm_detect(uint32_t window_in[WINDOW_SIZE]) {
                     if (0 == trans_state_cnt) {
                         beats_start[n_beats++] = i;
                     } 
-                    if (1 == trans_state_cnt) {
+                    if (64 == trans_state_cnt) {
                         state = SIGNAL;
                         trans_state_cnt = 0;
                     } else {
@@ -56,7 +69,7 @@ uint32_t bpm_detect(uint32_t window_in[WINDOW_SIZE]) {
                 break;
             case SIGNAL:
                 if (0 == window_in[i] ) {
-                    if (trans_state_cnt == 1) {
+                    if (32 == trans_state_cnt) {
                         state = WAIT_FOR_FIRST_SAMPLE;
                         trans_state_cnt = 0;
                     } else {
@@ -72,6 +85,7 @@ uint32_t bpm_detect(uint32_t window_in[WINDOW_SIZE]) {
         }
     }
     
+    return n_beats;
     uint32_t samples_per_beat = 0;
     if (n_beats > 3) {
         uint32_t n_diff = n_beats - 1;
@@ -80,6 +94,8 @@ uint32_t bpm_detect(uint32_t window_in[WINDOW_SIZE]) {
             sample_diff[i] = beats_start[i + 1] - beats_start[i];
         }
         uint32_t temp;
+        
+        samples_per_beat = n_beats;
         for (int i = 0; i < n_diff - 1; ++i) {
             for (int j = i + 1; j < n_diff; ++j) {
                 if (sample_diff[j] < sample_diff[i]) {
@@ -91,9 +107,9 @@ uint32_t bpm_detect(uint32_t window_in[WINDOW_SIZE]) {
         }
        
         samples_per_beat = (sample_diff[n_beats >> 2] +
-                sample_diff[(n_beats >> 2) + 1] +
-                sample_diff[(n_beats >> 2) - 1] + 
-                sample_diff[(n_beats >> 2) + 2]) >> 2; 
+                sample_diff[(n_beats >> 2) + 1]) >> 1;// +
+                /*sample_diff[(n_beats >> 2) - 1] + 
+                sample_diff[(n_beats >> 2) + 2]) >> 2; */
         
     }
     return samples_per_beat;
